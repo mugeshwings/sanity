1807173ebdf9b92f2e45c08dd7619ec11f67fe79
mugeshwings <mugeshkanna.wings@gmail.com>
2026-03-17 16:31:04 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./Components/HomePage";
-import "./Components/Home.css";
+import "./App.css";
 import Contact from "./Components/contact";
+import Insights from "./Components/insights/Insights";
+import InsightDetail from "./Components/insights/insightDetail";
+import About from "./Components/About/about";
+import Services from "./Components/Service/service";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/contact" element={<Contact />} />
+                <Route path="/insights" element={<Insights />} />
+                <Route path="/insight/:slug" element={<InsightDetail   />} />
+                  <Route path="/about" element={<About   />} />
+                   <Route path="/service" element={<Services   />} />
           </Routes>
         </main>
       </div>
